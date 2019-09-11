4016859296634ee06defc653827f5b3cec19c029
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-11 19:48:47 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -8,7 +8,7 @@ describe '#square_array' do
     square_array(numbers)
   end
 
-  it 'calls on each' do
+  Xit 'calls on each' do
     numbers = [1,2,3]
     expect(numbers).to receive(:each)
     square_array(numbers)
