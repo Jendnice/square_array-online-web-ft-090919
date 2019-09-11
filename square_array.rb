78a9fc8a3cf45eeb898ae94bc099076c32aee412
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-11 19:53:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
-  array.map do |number|
-    number ** 2
+  new_array = []
+  
+  array.each do |number|
+    new_array.push(number ** 2)
   end
+  new_array
 end
-
-array = [1, 2, 3]
